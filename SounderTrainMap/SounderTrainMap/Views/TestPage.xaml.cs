d2144e00fed407d56ff851ee0d7577bdb6e347d2
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-03-12 18:49:56 -0700

    Initialize project with Xamarin.Forms form.

---
+++ SounderTrainMap/SounderTrainMap/Views/TestPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SounderTrainMap.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        int curNum = 0;
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            curNum++;
+            NumberBox.Text = curNum.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            curNum--;
+            NumberBox.Text = curNum.ToString();
+        }
+    }
+}
